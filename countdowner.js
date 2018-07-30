241bce03b91ff74af7bc712d1551562a488411e9
Kenneth Murerwa <xwaxes@gmail.com>
2018-07-30 08:32:44 +0300

    Add files via upload

---
+++ countdowner.js
@@ -0,0 +1,35 @@
+var n = 4;
+var min = 0;
+var hour = 4;
+var zm = "",zs = "",zh = "";
+var x = setInterval(function(){
+		if (min <-0.99){
+			hour -= 1;
+			min = 59;
+		}
+		if (n <0){
+			min -= 1;
+			n = 59;
+		}
+		if (hour<10){
+			zh = "0"
+		}
+		else{
+			zh = "";
+		}
+		if (min<10){
+				zm = "0"
+			}
+		else{
+			zm = "";
+		}
+		if (n<10) {
+				zs = "0"
+			}
+		else{
+			zs = "";
+		}
+		document.getElementById("demo").innerHTML = zh +hour +":" +zm +min +":" +zs +n;
+		n = n-1;//Truncate this value to zero decimal places
+},1000);
+document.getElementById("demo").innerHTML = "00:00:00";
